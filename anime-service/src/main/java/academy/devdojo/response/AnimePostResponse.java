e7af26bdb17ae5d2719604c7196dcca144f072b7
danmotaa <daniel.mota@sempreceub.com>
2025-11-01 13:11:18 -0300

    video20: Exercicio04 -> MapStruct

---
+++ anime-service/src/main/java/academy/devdojo/response/AnimePostResponse.java
@@ -0,0 +1,13 @@
+package academy.devdojo.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AnimePostResponse {
+
+    private Long id;
+    private String name;
+}
