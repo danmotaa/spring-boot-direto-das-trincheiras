8029b849bd31b4a1c1d4ee225bf40c374830eec7
daniel.camara <daniel.camara@iti.gov.br>
2025-11-05 16:33:28 -0300

    Video22: DeleteMapping

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,7 +24,6 @@ public class ProducerController {
     @GetMapping
     public ResponseEntity<List<ProducerGetResponse>> listAll(@RequestParam(required = false) String name) {
         log.debug("Request to get all Producers {}", name);
-
 
         var producers = Producer.getProducers();
         var producerGetResponseList = MAPPER.toProducerGetResponseList(producers);
@@ -46,24 +46,6 @@ public class ProducerController {
         return ResponseEntity.ok(producerGetResponse);
     }
 
-//    @GetMapping
-//    public List<Producer> listAll(@RequestParam(required = false) String name) {
-//
-//        var producers = Producer.getProducers();
-//        if (name == null) {
-//            return producers;
-//        }
-//        return producers.stream().filter(producer -> producer.getName().equalsIgnoreCase(name)).toList();
-//    }
-//
-//    @GetMapping("{id}")
-//    public Producer findById(@PathVariable Long id) {
-//        return Producer.getProducers()
-//                .stream()
-//                .filter(producer -> producer.getId().equals(id))
-//                .findFirst().orElse(null);
-//    }
-
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-key")
     public ResponseEntity<ProducerGetResponse> save(@RequestBody ProducerPostRequest producerPostRequest, @RequestHeader HttpHeaders headers) {
@@ -75,5 +57,19 @@ public class ProducerController {
         Producer.getProducers().add(producer);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        log.debug("Request to delete producer by id {}", id);
+
+        var producerToDelete = Producer.getProducers()
+                .stream()
+                .filter(producer -> producer.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Producer not found"));
+
+        Producer.getProducers().remove(producerToDelete);
+        return ResponseEntity.noContent().build();
     }
 }
--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -1,7 +1,9 @@
 package academy.devdojo.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,9 +14,11 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Producer {
-
+    @EqualsAndHashCode.Include
     private Long id;
+    @JsonProperty("name")
     private String name;
     private LocalDateTime createdAt;
     private static List<Producer> producers = new ArrayList<>();
