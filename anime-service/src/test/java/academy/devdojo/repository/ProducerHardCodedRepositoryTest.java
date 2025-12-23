18b333b97ebc5906809cab873b4e8843eda6c7bb
daniel.camara <daniel.camara@iti.gov.br>
2025-12-23 16:11:41 -0300

    Video33: Testes Unitários - Repositório pt01

---
+++ anime-service/src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
@@ -0,0 +1,50 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.domain.Producer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class ProducerHardCodedRepositoryTest {
+
+    @InjectMocks
+    private ProducerHardCodedRepository repository;
+
+    @Mock
+    private ProducerData producerData;
+
+    private final List<Producer> producersList = new ArrayList<>();
+
+    @BeforeEach
+    void init(){
+
+        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
+        var witStudio = Producer.builder().id(2L).name("Wit Studios").createdAt(LocalDateTime.now()).build();
+        var studioGhibli = Producer.builder().id(3L).name("Studios Ghibli").createdAt(LocalDateTime.now()).build();
+
+        producersList.addAll(List.of(ufotable, witStudio, studioGhibli));
+
+    }
+
+    @Test
+    @DisplayName("findAll returns a list with all producers")
+    void findAll_ReturnAllProducers_WhenSuccess() {
+
+        BDDMockito.when(producerData.getProducers()).thenReturn(producersList);
+
+        var producers = repository.findAll();
+        Assertions.assertThat(producers).isNotNull().hasSize(producers.size());
+    }
+
+}
