78eb4f2e5b37e698021a2079d896d1ef9e1454ea
daniel.camara <daniel.camara@iti.gov.br>
2026-01-05 09:28:40 -0300

    Video39: - Exercício 10 - Testes Unitários - Service

---
+++ anime-service/src/test/java/academy/devdojo/service/AnimeServiceTest.java
@@ -0,0 +1,171 @@
+package academy.devdojo.service;
+
+import academy.devdojo.domain.Anime;
+import academy.devdojo.repository.AnimeHardCodedRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AnimeServiceTest {
+
+    @InjectMocks
+    private AnimeService service;
+
+    @Mock
+    private AnimeHardCodedRepository repository;
+
+    private List<Anime> animesList;
+
+    @BeforeEach
+    void init() {
+
+        var shingeki = Anime.builder().id(1L).name("Attack On Titan").build();
+        var blackCover = Anime.builder().id(2L).name("Black Cover").build();
+        var gachiakuta = Anime.builder().id(3L).name("Gachiakuta").build();
+
+        animesList = new ArrayList<>(List.of(shingeki, blackCover, gachiakuta));
+    }
+
+    @Test
+    @DisplayName("findAll returns a list with all animes")
+    @Order(1)
+    void findAll_ReturnAllAnimes_WhenSuccess() {
+
+        BDDMockito.when(repository.findAll()).thenReturn(animesList);
+
+        var animes = service.getAll(null);
+        Assertions.assertThat(animes).isNotNull().hasSameElementsAs(animesList);
+    }
+
+    @Test
+    @DisplayName("findAll returns list with found object when name exists")
+    @Order(2)
+    void findByName_ReturnsFoundAnimesInList_WhenNameIsFound() {
+        var anime = animesList.getFirst();
+        var expectedAnimesFound = singletonList(anime);
+
+        BDDMockito.when(repository.findByName(anime.getName())).thenReturn(expectedAnimesFound);
+
+        var animesFound = service.getAll(anime.getName());
+        Assertions.assertThat(animesFound).containsAll(expectedAnimesFound);
+    }
+
+    @Test
+    @DisplayName("findAll returns empty list when name is not found")
+    @Order(3)
+    void findByName_ReturnsEmptyList_WhenNameIsNotFound() {
+
+        var name = "not-found";
+
+        BDDMockito.when(repository.findByName(name)).thenReturn(emptyList());
+
+        var animes = service.getAll(name);
+        Assertions.assertThat(animes).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("findById returns a anime with given id")
+    @Order(4)
+    void findById_ReturnAnimeById_WhenSuccess() {
+
+        var expectedAnime = animesList.getFirst();
+        BDDMockito.when(repository.findById(expectedAnime.getId())).thenReturn(Optional.of(expectedAnime));
+
+        var animes = service.findByIdOrThrowNotFound(expectedAnime.getId());
+
+        Assertions.assertThat(animes).isEqualTo(expectedAnime);
+    }
+
+    @Test
+    @DisplayName("findById throws ResponseStatusException when anime is not found")
+    @Order(5)
+    void findById_ThrowsResponseStatusException_WhenAnimeIsNotFound() {
+
+        var expectedAnime = animesList.getFirst();
+        BDDMockito.when(repository.findById(expectedAnime.getId())).thenReturn(Optional.empty());
+
+        Assertions.assertThatException()
+                .isThrownBy(() -> service.findByIdOrThrowNotFound(expectedAnime.getId()))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+
+    @Test
+    @DisplayName("save creates a anime")
+    @Order(6)
+    void save_CreatesAnimes_WhenSuccessful() {
+        var animeToSave = Anime.builder().id(99L).name("Mappa").build();
+
+        BDDMockito.when(repository.save(animeToSave)).thenReturn(animeToSave);
+
+        var savedAnime = service.save(animeToSave);
+
+        Assertions.assertThat(savedAnime).isEqualTo(animeToSave).hasNoNullFieldsOrProperties();
+    }
+
+    @Test
+    @DisplayName("delete removes a anime")
+    @Order(7)
+    void delete_RemovesAnimes_WhenSuccessful() {
+
+        var animeToDelete = animesList.getFirst();
+        BDDMockito.when(repository.findById(animeToDelete.getId())).thenReturn(Optional.of(animeToDelete));
+
+        BDDMockito.doNothing().when(repository).delete(animeToDelete);
+
+        Assertions.assertThatNoException().isThrownBy(() -> service.delete(animeToDelete.getId()));
+    }
+
+    @Test
+    @DisplayName("delete throws ResponseStatusException when anime is not found")
+    @Order(8)
+    void delete_ThrowsResponseStatusException_WhenAnimeIsNotFound() {
+
+        var animeToDelete = animesList.getFirst();
+        BDDMockito.when(repository.findById(animeToDelete.getId())).thenReturn(Optional.empty());
+
+        Assertions.assertThatException()
+                .isThrownBy(() -> service.delete(animeToDelete.getId()))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+
+    @Test
+    @DisplayName("update update a anime")
+    @Order(9)
+    void update_UpdatesAnimes_WhenSuccessful() {
+
+        var animeToUpdate = animesList.getFirst();
+        animeToUpdate.setName("Aniplex");
+        BDDMockito.when(repository.findById(animeToUpdate.getId())).thenReturn(Optional.of(animeToUpdate));
+        BDDMockito.doNothing().when(repository).update(animeToUpdate);
+
+        Assertions.assertThatNoException().isThrownBy(() -> service.update(animeToUpdate));
+    }
+
+    @Test
+    @DisplayName("update throws ResponseStatusException when anime is not found")
+    @Order(10)
+    void update_ThrowsResponseStatusException_WhenAnimeIsNotFound() {
+        var animeToUpdate = animesList.getFirst();
+
+        BDDMockito.when(repository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.empty());
+
+        Assertions.assertThatException()
+                .isThrownBy(() -> service.update(animeToUpdate))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+}
